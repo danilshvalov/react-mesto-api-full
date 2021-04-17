44797f9fb840f56252ad149e355dd7ab9c774435
Danil Shvalov <shvalovich@gmail.com>
2021-04-17 11:49:47 +1000

    Исправлены ссылки на frontend

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const {requestLogger, errorLogger} = require('./middlewares/logger');
 const app = express();
 
 const {PORT = 3000} = process.env;
-const frontendLinks = /\.danilshvalov.mesto.nomoredomains.icu/;
+const frontendLinks = /.*\/\/*.danilshvalov.mesto.nomoredomains.icu/;
 const connectionLink = 'mongodb://localhost:27017/mestodb';
 
 mongoose.connect(connectionLink, {
